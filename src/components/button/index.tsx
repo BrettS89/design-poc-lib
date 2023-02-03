995bec60a386ef6404d319b0bbc1216db1c6ecb0
Brett Sodie <blsodie@gmail.com>
2023-02-03 17:07:47 -0500

    chore: add children to button

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,14 +1,14 @@
 import baseStyles from './styles';
 import theme from '../../styles/theme';
 
-const Button = ({ styles = {} }) => {
+const Button = ({ children, styles = {} }) => {
   //@ts-ignore
   const themeStyles = theme.button ?? {};
   const allStyles = { ...baseStyles._base, ...themeStyles, ...styles }
 
   return (
     <button style={allStyles}>
-      Click meee
+      {children}
     </button>
   );
 };
